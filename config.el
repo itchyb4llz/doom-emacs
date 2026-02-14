c5fd8c8df69a3ba4e62c350a8dcd7209bb8ab774
Jaj Dollesin <codewithjom@gmail.com>
2026-02-15 00:02:06 +0800

    new config.

---
+++ config.el
@@ -0,0 +1,192 @@
+;; -*- lexical-binding: t; -*-
+
+(add-to-list 'default-frame-alist '(width  . 120))
+(add-to-list 'default-frame-alist '(height . 64))
+
+(setq doom-theme 'doom-one)
+(setq doom-font (font-spec :family "Agave Nerd Font" :size 15))
+
+(setq display-line-numbers-type nil)
+(setq confirm-kill-emacs nil)
+
+;; ------ ORG
+(setq org-directory "~/org")
+
+(defun jd/org-mode-setup ()
+  (org-indent-mode)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+(defun jd/org-get-projects ()
+  "Return all headings from projects.org."
+  (when (file-exists-p "~/org/projects.org")
+    (with-current-buffer (find-file-noselect "~/org/projects.org")
+      (org-map-entries
+       (lambda ()
+         (org-get-heading t t t t))))))
+
+(after! org
+
+  (add-hook 'org-mode-hook #'jd/org-mode-setup)
+  (add-hook 'org-mode-hook #'hl-todo-mode)
+
+  (setq org-ellipsis "..."
+        org-hide-emphasis-markers t
+        org-src-fontify-natively t
+        org-fontify-quote-and-verse-blocks t
+        org-src-tab-acts-natively t
+        org-edit-src-content-indentation 2
+        org-startup-folded 'showeverything
+        org-cycle-separator-lines 2)
+
+  (setq org-agenda-files
+        '("~/org/archive.org"
+          "~/org/tasks.org"
+          "~/org/inbox.org"
+          "~/org/meetings.org"
+          "~/org/projects.org"))
+
+  (setq org-log-done 'time
+        org-log-into-drawer t
+        org-agenda-start-with-log-mode t
+        org-agenda-start-on-weekday 0)
+
+  (setq org-todo-keywords
+        '((sequence
+           "TODO(t)" "NEXT(n)" "IN-PROGRESS(i)" "WAIT(w@/!)" "REVIEW(v)"
+           "|" "DONE(d!)" "CANCELLED(c@)")
+          (sequence
+           "BACKLOG(b)" "PLANNED(p)" "READY(r)" "ACTIVE(a)" "HOLD(h@)"
+           "|" "COMPLETED(m!)")))
+
+  (defun jd/org-agenda-format-project (txt)
+    "Append the PROJECT property of the task to the agenda item."
+    (let ((proj (org-entry-get (get-text-property 0 'org-hd-marker txt) "PROJECT")))
+      (if proj
+          (concat txt " (" proj ")")
+        txt)))
+
+  ;; Refile
+  (setq org-refile-targets '((nil :maxlevel . 1)
+                             (org-agenda-files :maxlevel . 1))
+        org-outline-path-complete-in-steps nil
+        org-refile-use-outline-path t)
+
+  ;; Babel
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)))
+
+  (push '("conf-unix" . conf-unix) org-src-lang-modes)
+
+  ;; Evil keybindings
+  (evil-define-key '(normal insert visual) org-mode-map
+    (kbd "C-j") #'org-next-visible-heading
+    (kbd "C-k") #'org-previous-visible-heading
+    (kbd "M-j") #'org-metadown
+    (kbd "M-k") #'org-metaup)
+
+  ;; Faces
+  (set-face-attribute 'org-document-title nil
+                      :font "Agave Nerd Font"
+                      :weight 'bold
+                      :height 1.0)
+
+  (dolist (face '(org-level-1 org-level-2 org-level-3
+                   org-level-4 org-level-5 org-level-6
+                   org-level-7 org-level-8))
+    (set-face-attribute face nil
+                        :font "Agave Nerd Font"
+                        :weight 'medium
+                        :height 1.0))
+
+  (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+
+  ;; Structure templates
+  (require 'org-tempo)
+  (add-to-list 'org-structure-template-alist '("sh" . "src sh"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python"))
+)
+
+;; ------ EVIL ORG
+
+(use-package! evil-org
+  :after org
+  :hook ((org-mode . evil-org-mode)
+         (org-agenda-mode . evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+;; ------ ORG ROAM
+(defun jd/org-get-projects ()
+  "Return a list of project names from projects.org top-level headings."
+  (let ((file "~/org/projects.org")
+        projects)
+    (when (file-exists-p file)
+      (with-current-buffer (find-file-noselect file)
+        (org-element-map (org-element-parse-buffer) 'headline
+          (lambda (hl)
+            (when (= (org-element-property :level hl) 1)
+              (push (org-element-property :raw-value hl) projects))))))
+    (reverse projects))) ;; reverse to keep original order
+
+(use-package! org-roam
+  :custom
+  (org-roam-directory "~/org/")
+  (org-roam-completion-everywhere t)
+
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :if-new (file+head "${slug}.org"
+                         "#+title: ${title}\n#+author: Jaj Dollesin\n#+date: %U\n\n")
+      :unnarrowed t)
+
+     ("p" "project" entry
+      "* %^{Project Name}\n:PROPERTIES:\n:CREATED: %U\n:OWNER: Jaj Dollesin\n:REPO:%^{Repository}\n:END:\n** Details\n- Domain:\n- Server:%?"
+      :target (file "projects.org")
+      :unnarrowed t)
+
+     ("t" "Task" entry
+      "* TODO %^{Task}\n:PROJECT: %(completing-read \"Project: \" (jd/org-get-projects))\n%^G%?"
+      :target (file "tasks.org")
+      :prepend t
+      :empty-lines 1
+      :unnarrowed t)
+
+     ("m" "Meeting" entry
+      "* TODO %^{Meeting}\n:PROJECT: %^{Project}\nSCHEDULED: <%^{Date}>\n%?"
+      :target (file "meetings.org")
+      :unnarrowed t)
+
+     ("i" "inbox" entry
+      "* TODO %^{Task}\t%^G\n%?"
+      :target (file "inbox.org")
+      :unnarrowed t)))
+
+  :bind (("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n l" . org-roam-buffer-toggle))
+  :config
+  (org-roam-db-autosync-enable))
+
+;; ------ AUTO ARCHIVE
+
+(setq org-archive-location "~/org/archive.org::")
+(setq org-archive-mark-done nil)
+
+(defun jd/org-auto-archive-done-tasks ()
+  (when (and (buffer-file-name)
+             (string= (buffer-file-name)
+                      (expand-file-name "~/org/tasks.org"))
+             (string= org-state "DONE"))
+    (org-archive-subtree)))
+
+(add-hook 'org-after-todo-state-change-hook
+          #'jd/org-auto-archive-done-tasks)
